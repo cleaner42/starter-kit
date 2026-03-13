752ac8aec82a882afa64463dbc51bf590c9d7d02
Marcin Kumorek <Marcin.Kumorek@aptiv.com>
2026-03-13 12:30:26 +0100

    SSR data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
 import Heading from '@/components/ui/heading';
 
-export default function HomePage() {
+const getProjects = async () => {
+  // Simulate an API call with a random delay
+  const delay = Math.floor(Math.random() * (2000 - 100 + 1)) + 100;
+  await new Promise(resolve => setTimeout(resolve, delay));
+
+  // Implement your data fetching logic here
+  return {};
+};
+
+export default async function HomePage() {
+  // SSR
+  // const data = await getProjects();
+
   return (
     <main>
       <Heading>Hello World</Heading>
